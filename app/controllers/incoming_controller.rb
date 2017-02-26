fb96e0b70d64dda622b5c5e1477bd3c479a8d7e4
Logan Gingerich <logangingerich@yahoo.com>
2017-02-26 08:42:26 -0700

    mailgun setup test 2

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,22 +4,18 @@ class IncomingController < ApplicationController
 
   def create
     # Find the user by using params[:sender]
-    @user = User.find_by(email: params[:sender])
+    @user = User.find_by_email(params[:sender])
     # Find the topic by using params[:subject]
-    @topic = Topic.find_by(title: params[:subject])
+    @topic = Topic.find_or_create_by(title: params[:subject])
     # Assign the url to a variable after retreiving it from params["body-plain"]
-    @url = @topic.bookmarks.find_by(url: params["body-plain"])
+    @bookmark = @topic.bookmarks.find_or_create_by(url: params["body-plain"])
     # Check if user is nil, if so, create and save a new user
     if @user.nil?
       @user = User.create!(email: params[:sender], password: "password", password_confirmation: "password")
     end
     # Check if the topic is nil, if so, create and save a new topic
-    if @topic.nil?
-      @topic = Topic.create!(title: params[:subject])
-    end
     # Now that you're sure you have a valid user and topic, build and save a new bookmark
     # Assuming all went well.
-    @bookmark = @topic.bookmarks.new(url: @url)
     @bookmark.save
     head 200
   end
