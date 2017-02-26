4bd849645786e57e7a70fdf9269c97a7b6729e92
Logan Gingerich <logangingerich@yahoo.com>
2017-02-26 09:27:20 -0700

    Another mailgun test

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,25 +4,26 @@ class IncomingController < ApplicationController
 
   def create
     # Find the user by using params[:sender]
-    @user = User.find_by(email: params[:sender])
+    #@user = User.find_by(email: params[:sender])
     # Find the topic by using params[:subject]
     @topic = Topic.find_by(title: params[:subject])
+    @topic.save
     # Assign the url to a variable after retreiving it from params["body-plain"]
-    @url = @topic.bookmarks.find_by(url: params["body-plain"])
+    #@url = @topic.bookmarks.find_by(url: params["body-plain"])
     # Check if user is nil, if so, create and save a new user
-    if @user.nil?
-      @user = User.new(email: params[:sender], password: "password", password_confirmation: "password")
-      @user.save
-    end
+    #if @user.nil?
+      #@user = User.new(email: params[:sender], password: "password", password_confirmation: "password")
+      #@user.save
+    #end
     # Check if the topic is nil, if so, create and save a new topic
-    if @topic.nil?
-      @topic = Topic.new(title: params[:subject])
-      @topic.save
-    end
+    #if @topic.nil?
+      #@topic = Topic.new(title: params[:subject])
+      #@topic.save
+    #end
     # Now that you're sure you have a valid user and topic, build and save a new bookmark
     # Assuming all went well.
-    @bookmark = @topic.bookmarks.new(url: @url)
-    @bookmark.save
+    #@bookmark = @topic.bookmarks.new(url: @url)
+    #@bookmark.save
     head 200
   end
 end
