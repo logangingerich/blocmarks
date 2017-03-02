6830517d7358a80d18ee361a083310fa39adc8a5
Logan Gingerich <logangingerich@yahoo.com>
2017-03-01 19:30:05 -0700

    incoming controller fix

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -6,26 +6,22 @@ class IncomingController < ApplicationController
   def create
     # Find the user by using params[:sender]
     @user = User.find_by(email: params[:sender])
+
+    if @user.nil?
+      @user = User.new(email: params[:sender], password: "password", password_confirmation: "password")
+      @user.save
+    end
     # Find the topic by using params[:subject]
     @topic = Topic.find_by(title: params[:subject])
 
     if @topic.nil?
       @topic = Topic.new(title: params[:subject])
+      @topic.save
     end
-    
-    @topic.save
+
     # Assign the url to a variable after retreiving it from params["body-plain"]
-    @url = @topic.bookmarks.find_by(url: params["body-plain"])
-    # Check if user is nil, if so, create and save a new user
-    if @user.nil?
-      @user = User.new(email: params[:sender], password: "password", password_confirmation: "password")
-      @user.save
-    end
+    @topic.bookmarks.create(url: params["body-plain"])
 
-    @topic.save
-
-    @bookmark = @topic.bookmarks.new(url: @url)
-    @bookmark.save
     head 200
   end
 end
