b1cc6201ad08a802a309d5cb66c5d7d9037deca9
Logan Gingerich <logangingerich@yahoo.com>
2017-03-01 18:34:41 -0700

    env change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'floating-sands-93807.herokuapp.com/'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  config.raise_delivery_errors = true
 
 end
